9477c4b976ef1865e414fe27dfbf9dc8645b7f4d
Matthew Davis <matthew@matthewdavis.io>
2021-03-15 00:00:40 -0400

    bump

--- sidebars.js
+++ sidebars.js
@@ -2,21 +2,6 @@ module.exports = {
 
     someSidebar: {
 
-        Docusaurus: ['doc1', 'doc2', 'doc3'],
-        'Survival Guides': [
-            'survival-guides/docker',
-            'survival-guides/kubernetes'
-        ],
-        Features: ['mdx'],
-
-    },
-
-};
-
-module.exports = {
-
-    someSidebar: {
-
         'Getting Started': [
 
             'getting-started/home'
